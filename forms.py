eecd9b2748cd0301dc2acf3624e8cbe1f2f1715d
danielm <dm1905once@gmail.com>
2020-05-03 18:20:35 -0700

    Add cooklist form and length validators

--- forms.py
+++ forms.py
@@ -2,17 +2,20 @@
 
 from flask_wtf import FlaskForm
 # from models import nada
-from wtforms import StringField, PasswordField, SelectField, SelectMultipleField
+from wtforms import StringField, TextAreaField, PasswordField, SelectField, SelectMultipleField
 from wtforms.validators import DataRequired, Email, Length
+
+MSG_MAX_LENGTH_30 = 'Input should not exceed 30 characters'
+MSG_MAX_LENGTH_50 = 'Input should not exceed 50 characters'
 
 
 ### User forms
 class UserRegisterForm(FlaskForm):
     """Form for registering new users."""
-    email = StringField('E-mail', validators=[DataRequired(), Email()])
+    email = StringField('E-mail', validators=[DataRequired(), Email(), Length(max=50, message=MSG_MAX_LENGTH_50)])
     password = PasswordField('Password', validators=[Length(min=6)])
-    first_name = StringField('First Name', validators=[DataRequired()])
-    last_name = StringField('Last Name')
+    first_name = StringField('First Name', validators=[DataRequired(), Length(max=30, message=MSG_MAX_LENGTH_30)])
+    last_name = StringField('Last Name', validators=[Length(max=30, message=MSG_MAX_LENGTH_30)])
     measure_system = SelectField('Measure System', choices=[('metric','Metric'), ('us', 'US')])
 
 class UserLoginForm(FlaskForm):
@@ -29,3 +32,10 @@ class SearchByMealTypeForm(FlaskForm):
 class SearchByIngredientsForm(FlaskForm):
     """Form to search for recipes based on ingredients."""
     ingredients = SelectMultipleField('Search by Main Ingredients')
+
+### Cooklist form
+class CooklistForm(FlaskForm):
+    """Form for adding cooklists."""
+
+    list_name   = StringField("Cooklist Name", validators=[DataRequired(), Length(max=30, message=MSG_MAX_LENGTH_30)])
+    description = TextAreaField("Description", validators=[Length(max=50, message=MSG_MAX_LENGTH_50)])
